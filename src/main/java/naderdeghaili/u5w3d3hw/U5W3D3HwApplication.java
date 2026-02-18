deab04f7d8e7879c1a4e59a22f361304c96f7378
NAD3R <naderdeghaili@gmail.com>
2026-02-18 13:12:52 +0100

    initial commit

---
+++ src/main/java/naderdeghaili/u5w3d3hw/U5W3D3HwApplication.java
@@ -0,0 +1,13 @@
+package naderdeghaili.u5w3d3hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D3HwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W3D3HwApplication.class, args);
+    }
+
+}
